c2ed174ebb955ecd420bf8d9f07dd8ae770dc55c
Ponmani <ponmani529@gmail.com>
2021-01-20 22:58:59 +0530

    day 91 added

---
+++ Day91JavacriptDrumkit/app.js
@@ -0,0 +1,18 @@
+
+window.addEventListener('keydown',(event)=>{
+    //  console.log(event.keyCode);
+    const audio=document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key=document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime=0;
+    audio.play();
+    key.classList.add('playing');
+    // console.log(audio);
+})
+
+function removeTransition(e){
+  if(e.propertyName!=='transform') return;
+  this.classList.remove('playing');
+}
+const keys=document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
